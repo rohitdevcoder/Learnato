6967d17c63c2e94c5642ce5634bf1145a9318d44
rohitdevcoder <rohitdevlink98@gmail.com>
2025-11-09 16:46:48 +0530

    error solved

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import authUser from '../middleware/authUser.js';
+// import authUser from '../middleware/authUser.js';
 import { isAuth, loginUser, registerUser } from '../controllers/authController.js';
 
 const UserRouter = express.Router();
 
 UserRouter.post('/register', registerUser)
 UserRouter.post('/login', loginUser);
-UserRouter.get('/is-auth',authUser,isAuth);
+// UserRouter.get('/is-auth',authUser,isAuth);
 
 export default UserRouter;
